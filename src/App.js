ee61bc029c996efe3453afbabeb1e1e432f3e539
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-12-09 18:27:06 -0300

    Added login, fetch planes, fetch reservations
    Not the final product

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchPlanes, selectAllPlanes, selectPlaneById } from './redux/slices/planesSlice';
+import Cookies from 'universal-cookie';
+import {
+  selectAllPlanes, selectPlaneById, logIn, fetchPlanes,
+  fetchReservations,
+} from './redux/slices/planesSlice';
 import './App.css';
 
 function App() {
   const dispatch = useDispatch();
   const { entities } = useSelector((state) => state.planes);
-
-  useEffect(() => {
-    dispatch(fetchPlanes());
-  }, [dispatch]);
+  const biscoitos = new Cookies();
+  // useEffect(() => {
+  //   dispatch(fetchPlanes());
+  // }, [dispatch]);
   console.log(entities);
   return (
     <div className="App">
       <p>IHUL: </p>
-      <p>{JSON.stringify(useSelector((state) => selectPlaneById(state, 1)))}</p>
+      {/* <p>{JSON.stringify(useSelector((state) => selectPlaneById(state, 1)))}</p> */}
+      <button type="button" onClick={() => dispatch(logIn())}>Login</button>
+      <button type="button" onClick={() => console.log(biscoitos.get('MyToken'))}>Token Cookie</button>
+      <button type="button" onClick={() => dispatch(fetchPlanes())}>Fetch plane</button>
+      <button type="button" onClick={() => dispatch(fetchReservations())}>Fetch Reservations</button>
     </div>
   );
 }
--- src/redux/slices/planesSlice.js
+++ src/redux/slices/planesSlice.js
@@ -5,17 +5,45 @@ import {
   createSlice,
 } from '@reduxjs/toolkit';
 import axios from 'axios';
+import Cookies from 'universal-cookie';
 
-export const planesAdapter = createEntityAdapter();
+export const planesAdapter = createEntityAdapter({
+  selectId: (plane) => plane.id,
+});
 
 const initialState = planesAdapter.getInitialState({ status: 'idle' });
 
-export const fetchPlanes = createAsyncThunk('planes/fetchPlane', async () => {
-  console.log('entrou?');
-  const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
-  console.log('ha entro mermo');
+export const logIn = createAsyncThunk('api/login', async () => {
+  const { headers: { authorization } } = await axios.post('https://air-taxi.herokuapp.com/users/sign_in', {
+    user: {
+      email: '12345@gmail.com',
+      password: '123456',
+    },
+  });
+  const cookies = new Cookies();
+  cookies.set('MyToken', authorization, { path: '/' });
+});
+
+export const fetchPlanes = createAsyncThunk('api/planes', async () => {
+  const bolacha = new Cookies();
+  const { data } = await axios.get('https://air-taxi.herokuapp.com/api/v1/users/id/planes', {
+    headers: {
+      Authorization: `${bolacha.get('MyToken')}`,
+    },
+  });
   console.log(data);
   return data;
+});
+
+export const fetchReservations = createAsyncThunk('api/reservations', async () => {
+  const bolacha = new Cookies();
+  // the id of the plane must be informed, the 1 is just a place holder
+  const response = await axios.get('https://air-taxi.herokuapp.com/api/v1/users/id/planes/1/reservations', {
+    headers: {
+      Authorization: `${bolacha.get('MyToken')}`,
+    },
+  });
+  console.log(response);
 });
 
 const planesSlice = createSlice({
