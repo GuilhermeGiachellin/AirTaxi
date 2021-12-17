391fe96e57de24dda178aff4a5fe4a59a94fc1cb
Gabriel <gabriel.gabrielyea@gmail.com>
2021-12-17 14:41:59 -0600

    Remove navbar from component

--- src/component/nav/navBar.js
+++ src/component/nav/navBar.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logOut } from '../../redux/slices/sessionSlice';
-import styles from '../../assets/navBar.module.css';
+import styles from './navBar.module.scss';
 
 const NavBar = () => {
   const dispatch = useDispatch();
   return (
 
-    <div className={styles.nav_cnt}>
+    <div className={styles.mainContainer}>
       <h1>LOGO</h1>
-      <nav className={styles.nav_list}>
+      <nav className={styles.itemList}>
         <Link to="/main" className={styles.nav_link}>PLANES</Link>
         <Link to="/add" className={styles.nav_link}>ADD PLANE</Link>
         <Link to="/" onClick={() => dispatch(logOut())} className={styles.nav_link}>SIGN OUT</Link>
