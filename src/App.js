00cfe00e0b1e750fccc99b1dd225385cdce6cd8c
Gabriel <gabriel.gabrielyea@gmail.com>
2021-12-17 14:24:35 -0600

    Arrange pages folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import {
-  Routes, Route, Link, useNavigate,
+  Routes, Route, useNavigate,
 } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import styles from './assets/app.module.css';
-import LogInForm from './component/myForm/LogInForm';
-import SigUpForm from './component/myForm/SignUpForm';
 import CreatePlaneForm from './component/myForm/CreatePlaneForm';
 import Reservation from './component/ReservationManager';
 import PlaneDetails from './component/plane/planeDetails';
-import Login from './component/pages/Login';
-import MainView from './component/pages/MainView';
+import Login from './pages/Login';
+import MainView from './pages/MainView';
 
 function App() {
   const status = useSelector((state) => state.sessions.status);
