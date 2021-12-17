01f74acf3c9a478c0f3ec062ce04b99eb915cde3
Gabriel <gabriel.gabrielyea@gmail.com>
2021-12-17 14:25:41 -0600

    Move Files to pages folder

---
+++ src/pages/MainView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PlaneManager from '../component/PlaneManager';
+import NavBar from '../component/nav/navBar';
+// import styles from './mainView.module.scss';
+
+const MainView = () => (
+  <div>
+    <NavBar />
+    <PlaneManager />
+  </div>
+);
+
+export default MainView;
