cf2f89486dec9b1beb49671b9363b5d604a68f68
Gabriel <gabriel.gabrielyea@gmail.com>
2021-12-21 15:08:53 -0600

    Create popup

---
+++ src/component/popups/PopUp.js
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+import { motion } from 'framer-motion';
+import React, { useState } from 'react';
+import styles from './popup.module.scss';
+
+const PopUp = ({ handleInput, message, skip }) => {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <>
+      {open && (
+      <motion.div
+        className={styles.mainContainer}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      >
+        <motion.div
+          className={styles.options}
+          initial={{ y: -1000, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+        >
+          <h3>{message}</h3>
+          <button type="button" onClick={() => handleInput()}>Go to main</button>
+          {skip && (
+          <button type="button" onClick={() => setOpen(false)}>Close</button>
+          )}
+        </motion.div>
+      </motion.div>
+      )}
+    </>
+  );
+};
+
+export default PopUp;
