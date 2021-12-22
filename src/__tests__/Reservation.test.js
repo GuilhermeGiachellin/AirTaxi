767689503972f9504515d098d08f7ca266786577
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-12-22 17:29:38 -0300

    Add reservation page tests

---
+++ src/__tests__/Reservation.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+import '@testing-library/jest-dom/extend-expect';
+import Reservation from '../pages/Reservation';
+
+describe('Test Reservation', () => {
+  const viewRender = () => {
+    const component = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Reservation />
+        </Provider>
+        ,
+      </BrowserRouter>,
+    );
+    return component;
+  };
+
+  let component;
+  beforeEach(() => {
+    component = viewRender();
+  });
+
+  test('Render reservation title', async () => {
+    const { getByText } = component;
+    expect(getByText('BOOK YOUR PLANE')).toBeInTheDocument();
+  });
+
+  test('Render reservation button 1', async () => {
+    const { getByText } = component;
+    expect(getByText('Reserved dates')).toBeInTheDocument();
+  });
+
+  test('Render reservation button 2', async () => {
+    const { getByText } = component;
+    expect(getByText('Book now')).toBeInTheDocument();
+  });
+});
+
+describe('Sanpshoot of the Reservation page', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Reservation />
+        </Provider>
+        ,
+      </BrowserRouter>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
