62f9b55481aa2d6399485a168061f26a3d395b1f
Gabriel <gabriel.gabrielyea@gmail.com>
2021-12-23 22:36:07 -0600

    Add mobile version for main view and popup

--- src/pages/MainView.js
+++ src/pages/MainView.js
@@ -2,10 +2,11 @@ import React from 'react';
 import PlaneManager from '../component/managers/PlaneManager';
 import NavBar from '../component/nav/navBar';
 import Layout from '../component/wrapper/Layout';
+import styles from './pages.module.scss';
 
 const MainView = () => (
   <Layout preAnimation="fadeIn">
-    <div style={{ display: 'flex', flexDirection: 'row', width: '100vw' }}>
+    <div className={styles.pageContainer}>
       <NavBar />
       <PlaneManager />
     </div>
