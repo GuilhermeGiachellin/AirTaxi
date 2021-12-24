e70984eb534421bf5d3db9504896062322db7496
Gabriel <gabriel.gabrielyea@gmail.com>
2021-12-23 22:35:51 -0600

    Add mobile version for details

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -2,10 +2,11 @@ import React from 'react';
 import NavBar from '../component/nav/navBar';
 import PlaneDetails from '../component/plane/planeDetails';
 import Layout from '../component/wrapper/Layout';
+import styles from './pages.module.scss';
 
 const Details = () => (
   <Layout preAnimation="fadeIn">
-    <div style={{ display: 'flex', flexDirection: 'row', width: '100vw' }}>
+    <div className={styles.pageContainer}>
       <NavBar />
       <PlaneDetails />
     </div>
